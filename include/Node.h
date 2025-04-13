3fdfc45abece8c8d11a7d61bca40625f9ea15116
AmanMehrishi <f20220068@pilani.bits-pilani.ac.in>
2025-04-14 03:08:53 +0530

    added node base class

---
+++ include/Node.h
@@ -0,0 +1,25 @@
+#ifndef NODE_H
+#define NODE_H
+
+
+#include <string.h>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+class Node{
+    public:
+        Node(const string& id) : id(id) {}
+        virtual ~Node() {}
+        virtual void process() = 0;
+        string getId() const {return id;}
+
+        
+    protected:
+        string id;
+};
+
+
+
+
+#endif
