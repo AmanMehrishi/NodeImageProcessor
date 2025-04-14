5f78868b4ce987aa6926992b7ccdc59ddf3d7f20
AmanMehrishi <f20220068@pilani.bits-pilani.ac.in>
2025-04-14 20:07:57 +0530

    updated ImageInputNode.cpp to display metadata

--- src/ImageInputNode.cpp
+++ src/ImageInputNode.cpp
@@ -1,6 +1,7 @@
 #include "ImageInputNode.h"
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <filesystem>
 
 
 using namespace std;
@@ -13,5 +14,14 @@ void ImageInputNode::process(){
         cin.get();
     }else {
         cout<<"Loaded Image: "<< imagePath <<endl;
+        cout<<" - Dimensions: " << output.cols << " x " << output.rows << endl;
+        try{
+            auto fsize = filesystem::file_size(imagePath);
+            cout<<" - File Size: "<<fsize<<" bytes"<<endl;
+        } catch(...){
+            cout<<" - File size unavailable"<<endl;
+        }
     }
+    filesystem::path p(imagePath);
+    cout<<" - Format: "<<p.extension().string()<<endl;
 }
